fb059e9654ea189e81a5415423aeab9d9ac671dd
daneilhpp <carvalhosilvad88@gmail.com>
2022-06-01 22:40:50 -0300

    Project Upload

---
+++ app/src/main/java/com/daneil/words/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.daneil.words
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.daneil.words.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = LetterAdapter()
+    }
+}
